6dec5885f5e04cd2f3c1d7b6ee6122fa59227270
Helen Miller <hmiller@fredhutch.org>
2021-07-20 15:43:18 -0700

    Set credential for test

--- tests/testthat/helper-setup.R
+++ tests/testthat/helper-setup.R
@@ -6,4 +6,15 @@ suppressPackageStartupMessages({
   library(testthat)
 })
 
+
+# Declare global test variables ------------------------------------------------
+
+# Move netrc file out the way to test against login and password in .Renviron
+if (!any(file.exists("~/.netrc", "~/_netrc"))) {
+  assign("labkey.netrc.file", ImmuneSpaceR:::.get_env_netrc(), .GlobalEnv)
+}
+
+# Regardless of netrc presence, test against `ISR_machine` in .Renviron
+assign("labkey.url.base", ImmuneSpaceR:::.get_env_url(), .GlobalEnv)
+
 con_all <- CreateConnection("")
